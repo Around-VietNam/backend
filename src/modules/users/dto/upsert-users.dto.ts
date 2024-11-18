4d78fc7fdb9d394f12324726dffeb94a733b6a11
trungnotchung <trung09072004@gmail.com>
2024-11-18 23:37:37 +0700

    add user dto

---
+++ src/modules/users/dto/upsert-users.dto.ts
@@ -0,0 +1,65 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String })
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String })
+  gender: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  @ApiProperty({ type: Date })
+  date_of_birth: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String })
+  location: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String })
+  avt: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String })
+  hash_password: string;
+}
+
+export class CreateUsersDto extends UpdateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String })
+  user_name: string;
+}
+
+export class UserFeedbackDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String })
+  user_name: string;
+
+  @IsString()
+  @ApiProperty({ type: String })
+  restaurantFeedback: string;
+
+  @IsString()
+  @ApiProperty({ type: String })
+  dishFeedback: string;
+
+  @IsString()
+  @ApiProperty({ type: String })
+  landmarkFeedback: string;
+}
