3a2866e17948a32f346093d7fb276e05d15c7400
trungnotchung <trung09072004@gmail.com>
2024-11-24 10:23:09 +0700

    feat: update user info by username

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserResponseDto } from './dto/response-users.dto';
-import { CreateUsersDto } from './dto/upsert-users.dto';
+import { CreateUsersDto, UpdateUserDto } from './dto/upsert-users.dto';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 
@@ -28,5 +28,18 @@ export class UsersController {
   @Get(':username')
   async getUserDetails(@Param('username') username: string): Promise<User> {
     return this.service.findByUserName(username);
+  }
+
+  @ApiResponse({
+    status: 200,
+    type: UserResponseDto,
+    description: 'Update User Info',
+  })
+  @Put(':username')
+  async updateUser(
+    @Param('username') username: string,
+    @Body() dto: UpdateUserDto,
+  ): Promise<User> {
+    return this.service.updateUser(username, dto);
   }
 }
