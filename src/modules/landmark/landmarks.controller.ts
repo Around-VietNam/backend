c6a176a0649d022e24d6d0dd4b51add5a1b048e2
trungnotchung <trung09072004@gmail.com>
2024-11-30 20:38:01 +0700

    feat: get landmark feedbacks

--- src/modules/landmark/landmarks.controller.ts
+++ src/modules/landmark/landmarks.controller.ts
@@ -1,14 +1,28 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { LandmarksService } from './landmark.service';
 import { LandmarksResponseDto } from './dtos/response-landmarks.dto';
 import { UpsertLandmarksDto } from './dtos/upsert-landmarks.dto';
 import { Landmark } from './entities/landmark.entity';
+import { GetManyLandmarkFeedbacksResponseDto } from './dtos/response-landmark-feedbacks.dto';
+import { QueryLandmarkFeedbacksDto } from './dtos/query-landmark-feedbacks.dto';
+import { LandmarkFeedbacksService } from './landmark-feedbacks.service';
 
 @ApiTags('Landmarks')
 @Controller('landmarks')
 export class LandmarksController {
-  constructor(public landmarkService: LandmarksService) {}
+  constructor(
+    public landmarkService: LandmarksService,
+    public landmarkFeedbackService: LandmarkFeedbacksService,
+  ) {}
 
   @ApiResponse({
     status: 201,
@@ -46,5 +60,18 @@ export class LandmarksController {
   @Delete(':id')
   async deleteLandmark(@Param('id') id: number): Promise<void> {
     return this.landmarkService.deleteLandmark(id);
+  }
+
+  @ApiResponse({
+    status: 200,
+    type: GetManyLandmarkFeedbacksResponseDto,
+    description: 'Get restaurant feedbacks',
+  })
+  @Get(':id/feedbacks')
+  async getFeedbacksByRestaurantId(
+    @Param('id') id: number,
+    @Query() query: QueryLandmarkFeedbacksDto,
+  ): Promise<GetManyLandmarkFeedbacksResponseDto> {
+    return this.landmarkFeedbackService.getFeedbacksByLandmarkId(id, query);
   }
 }
--- src/modules/landmark/landmarks.module.ts
+++ src/modules/landmark/landmarks.module.ts
@@ -3,11 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Landmark } from './entities/landmark.entity';
 import { LandmarksController } from './landmarks.controller';
 import { LandmarksService } from './landmark.service';
+import { LandmarkFeedback } from './entities/feedback.entity';
+import { User } from '../users/entities/user.entity';
+import { LandmarkFeedbacksService } from './landmark-feedbacks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Landmark])],
+  imports: [TypeOrmModule.forFeature([Landmark, LandmarkFeedback, User])],
   controllers: [LandmarksController],
-  providers: [LandmarksService],
-  exports: [LandmarksService],
+  providers: [LandmarksService, LandmarkFeedbacksService],
+  exports: [LandmarksService, LandmarkFeedbacksService],
 })
 export class LandmarksModule {}
