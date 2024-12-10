1398779a8ebbd5db1388d75c8adfe8178bc9db0c
trungnotchung <trung09072004@gmail.com>
2024-12-10 12:24:15 +0700

    add dishes action

--- src/modules/dishes/dishes.controller.ts
+++ src/modules/dishes/dishes.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { DishesService } from './dishes.service';
 import { DishesResponseDto } from './dtos/response-dishes.dto';
@@ -18,5 +18,33 @@ export class DishesController {
   @Post()
   async createDish(@Body() dto: UpsertDishesDto): Promise<Dish> {
     return this.dishesService.create(dto);
+  }
+
+  @ApiResponse({
+    status: 200,
+    type: DishesResponseDto,
+    description: 'Get dish details',
+  })
+  @Get(':id')
+  async getDish(@Param('id') id: number): Promise<Dish> {
+    return this.dishesService.getDishById(id);
+  }
+
+  @ApiResponse({
+    status: 200,
+    type: DishesResponseDto,
+    description: 'Update dish details',
+  })
+  @Post(':id')
+  async updateDish(
+    @Param('id') id: number,
+    @Body() dto: UpsertDishesDto,
+  ): Promise<Dish> {
+    return this.dishesService.updateDish(id, dto);
+  }
+
+  @Delete(':id')
+  async deleteDish(@Param('id') id: number): Promise<void> {
+    return this.dishesService.deleteDish(id);
   }
 }
--- src/modules/dishes/dishes.service.ts
+++ src/modules/dishes/dishes.service.ts
@@ -54,4 +54,28 @@ export class DishesService
     await dish.save({ reload: true });
     return dish;
   }
+
+  async updateDish(id: number, dto: UpsertDishesDto): Promise<Dish> {
+    const dish = await this.getDishById(id);
+    const restaurant = await this.repository.manager.findOne(Restaurant, {
+      where: { id: dto.restaurantId },
+    });
+    if (!restaurant) {
+      throw new Error('Restaurant not found');
+    }
+
+    dish.name = dto.name;
+    dish.description = dto.description;
+    dish.image = dto.image;
+    dish.price = dto.price;
+    dish.special = dto.special;
+    dish.restaurant = restaurant;
+    await dish.save({ reload: true });
+    return dish;
+  }
+
+  async deleteDish(id: number): Promise<void> {
+    const dish = await this.getDishById(id);
+    await dish.remove();
+  }
 }
