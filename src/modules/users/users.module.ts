27af3dff0a60c5fb18ef39cdaea62c91e00b5910
ttb06 <trantuanbinhuet@gmail.com>
2024-12-14 18:06:15 +0700

    feat: favorite (#16)
    * update user service
    
    * feat: favorite landmarks
    
    * add favorite dishes service
    
    * feat: favorite dishes

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,9 +4,11 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/modules/users/entities/user.entity';
 import { Restaurant } from '../restaurants/entities/restaurant.entity';
+import { Landmark } from '../landmark/entities/landmark.entity';
+import { Dish } from '../dishes/entities/dish.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Restaurant])],
+  imports: [TypeOrmModule.forFeature([User, Restaurant, Landmark, Dish])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
